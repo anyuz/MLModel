2c94cd406b999c9613ef9db76a623574434762e9
anyuz <anyuz@smu.edu>
2019-03-03 14:57:06 -0800

    Update model.py

--- model.py
+++ model.py
@@ -15,6 +15,9 @@ import argparse
 import sys
 import boto3
 import botocore
+import datetime
+
+currentDT = datetime.datetime.now()
 
 # argument
 parser = argparse.ArgumentParser(description="training model")
@@ -542,7 +545,7 @@ def main(debug=False):
         kfold_lightgbm(train_df, test_df, num_folds=2, stratified=False, debug=debug)
     # insert results to database
     # db.accuracyround.insertOne({ model: "model"+str(avg(predictresults)), Fold100: predictresults[0], Fold200: predictresults[1], Fold300: predictresults[2], Fold400: predictresults[3], Fold500: predictresults[4], Fold600: predictresults[5], Fold700: predictresults[6], Fold800: predictresults[7],Fold900: predictresults[8], Fold1000: predictresults[9]})
-    db.model.insertOne({ model: "model"+str(avg(predictresults)), accuracy: predictresults[0]})
+    db.accuracy.insertOne({ model: "model"+str(currentDT), accuracy: predictresults[0]})
 
 if __name__ == "__main__":
     with timer("Full model run"):
