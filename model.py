54831df2bf8974e0edff7bbe51b3a4462de37005
anyuz <anyuz@smu.edu>
2019-03-03 18:17:05 -0800

    Update model.py

--- model.py
+++ model.py
@@ -25,7 +25,7 @@ currentDT = datetime.datetime.now()
 # def test_answer():
 #     assert func(3) == 4
 with open('results.txt','a') as fd:
-        fd.write([str(currentDT), 4.5])
+        fd.write(4.5)
 # argument
 parser = argparse.ArgumentParser(description="training model")
 parser.add_argument("--mongodb")
@@ -563,8 +563,9 @@ def main(debug=False):
     # insert results to database
     # db.accuracyround.insertOne({ model: "model"+str(avg(predictresults)), Fold100: predictresults[0], Fold200: predictresults[1], Fold300: predictresults[2], Fold400: predictresults[3], Fold500: predictresults[4], Fold600: predictresults[5], Fold700: predictresults[6], Fold800: predictresults[7],Fold900: predictresults[8], Fold1000: predictresults[9]})
     accuracy.insert({"model":"model"+ str(currentDT), "accuracy": predictresults[0]})
-    with open('results.txt','a') as fd:
-        fd.write([old_accuracy, predictresults[0]])
+    with open('results.txt','w') as fd:
+        fd.write(old_accuracy)
+        fd.write(predictresults[0])
     
 
 if __name__ == "__main__":
